bf92cfb2d5fb63f56e9149bd236934edfce33e3a
Saulcidseijas <saulcidseijas@gmail.com>
2025-04-27 20:05:19 +0200

    Engado a base da web

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="gal">    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>O meu portafolio</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>Benvido ao meu portafolio</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#about">Sobre min</a></li>
+                <li><a href="#contact">Contacto</a></li>
+            </ul>
+        </nav>
+        <main>
+            <section id="home">
+                <h2>Inicio</h2>
+                <p>Esta é a sección principal</p>
+            </section>
+            <section id="about">
+                <h2>Sobre min</h2>
+                <p>Esta é a seccióm sobre min</p>
+            </section>
+            <section id="contact">
+                <h2>Contacto</h2>
+                <p>Esta é a sección de contacto.</p>
+            </section>
+        </main>
+        <footer>
+            <p>A miña web 2025</p>
+        </footer>
+
+</html>
+
